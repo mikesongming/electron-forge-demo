cebefe504b14edc462f83ddcba62408e32173845
Mike Song <mikesongming@gmail.com>
2022-08-31 09:01:54 +0800

    preload trial

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,27 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) {
+            element.innerText = text;
+        }
+    };
+
+    for (const type of ["chrome", "node", "electron"]) {
+        replaceText(`${type}-version`, process.versions[type]);
+    }
+});
+
+
+// BUG: when import contextBridge, "Unable to load preload script" 
+//
+// import { contextBridge } from "electron";
+// 
+// contextBridge.exposeInMainWorld("versions", {
+//     chrome: process.versions["chrome"],
+//     node: process.versions["node"],
+//     electron: process.versions["electron"],
+// })
